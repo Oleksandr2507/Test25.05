e2a6e7165cf1d188c24e7334e9978b56de6c7af4
Oleksandr <Oleksandr@weltenbauer-se.com>
2016-05-25 14:32:46 +0200

    Many new tasks in gulpfile.js and corresponding files

---
+++ public/assets/javascript/bundle.js
@@ -0,0 +1,2 @@
+console.log(1);
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImpzMS5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQSIsImZpbGUiOiJidW5kbGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygxKTsiXSwic291cmNlUm9vdCI6Ii9zb3VyY2UvIn0=
